5d5474edf201cb721370873a6761eb1a3f3a3ac3
rjnogales <rnogales@hprn>
2023-07-24 15:33:55 -0500

    codigo inicial

---
+++ src/views/ErrorDisplay.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h4>Oops! There was an error:</h4>
+        <p>{{ error }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['error']
+}
+</script>
